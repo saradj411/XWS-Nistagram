1c5ce65979f36f872cae87dd0f5b6136d6354259
saradj411 <saradjuric98@yahoo.com>
2021-06-18 15:05:57 +0200

    autorizacija

--- media-microservice/src/main/java/com/example/mediamicroservice/MediaMicroserviceApplication.java
+++ media-microservice/src/main/java/com/example/mediamicroservice/MediaMicroserviceApplication.java
@@ -4,11 +4,15 @@ package com.example.mediamicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 //@SpringBootApplication
 @EnableScheduling
+@EnableEurekaClient
+@EnableFeignClients
 public class MediaMicroserviceApplication {
 
 	public static void main(String[] args) {
--- profile-microservice/src/main/java/com/example/profilemicroservice/ProfileMicroserviceApplication.java
+++ profile-microservice/src/main/java/com/example/profilemicroservice/ProfileMicroserviceApplication.java
@@ -3,12 +3,15 @@ package com.example.profilemicroservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 //@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @SpringBootApplication
 @EnableScheduling
-
+@EnableEurekaClient
+@EnableFeignClients
 public class ProfileMicroserviceApplication {
 
 	public static void main(String[] args) {
